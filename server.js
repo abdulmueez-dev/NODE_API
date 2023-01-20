295c0ca9e032891167a88d0a8251023833fae4d2
mueez <mueezs005@gmail.com>
2023-01-20 16:45:51 +0500

    Authentication Done

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const mongoose =require("mongoose");
 const cookieParser = require('cookie-parser')
 
 // CONNECTING TO DATABASE
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true });
-const db=mongoose.connection
-db.on('error',(error) => console.log(error))
-db.once('open', () => console.log('Connected to DB'))
+// mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true });
+// const db=mongoose.connection
+// db.on('error',(error) => console.log(error))
+// db.once('open', () => console.log('Connected to DB'))
+const DB=process.env.DATABASE_URL
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+}).then(() =>{
+     console.log("Connected to database");
+}).catch((err)=>console.log(err));
+
 
 // ACCEPTING JSON IN BODY
 app.use(express.json());
@@ -20,4 +30,5 @@ app.use('/users',usersRouter)
 
 
 // SET LISTENING PORT
-app.listen(3000,() =>(console.log("Listening on port 3000")));
+const PORT= process.env.PORT || 3000
+app.listen(PORT,() =>(console.log(`Listening on port ${PORT} `)));
